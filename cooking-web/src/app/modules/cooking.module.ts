d2c81d197280392151dfe5fad3d79077d2267e3d
Wojtek <matrix890098@gmail.com>
2021-03-21 11:04:05 +0100

    Dodanie angular materials

---
+++ cooking-web/src/app/modules/cooking.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+
+const MaterialComponents = [
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule
+  ]
+})
+export class CookingModule { }
