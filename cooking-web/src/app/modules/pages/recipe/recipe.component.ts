d5c689dfe93b3a48769208bd69659f7cb7931aec
Mateusz <mbagnicki7@gmail.com>
2021-03-25 23:34:32 +0100

    Działający szkielet strony z przepisami

--- cooking-web/src/app/modules/pages/recipe/recipe.component.ts
+++ cooking-web/src/app/modules/pages/recipe/recipe.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
@@ -9,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 
-  constructor() { }
+  recipe: any;
 
-  ngOnInit(): void {
+  constructor() { 
+
+    this.recipe = {
+      cusine_title: "Rice with chicken",
+      rating_short: "4.5 (36 reviews)",
+      cusine_type: "Asian",
+      meal_type: "Dinner",
+      difficulty: 3,
+      duration: "45min",
+      calories: "520",
+      servings: "6",
+
+      ingredients: [
+        '240 grams',
+        "79 militers sour cream",
+        "2 teaspoons",
+        "2 teaspoons",
+        "1/2 teaspon",
+        "454 grams chicken",
+        "321 grams cauliflowe rice",
+        "113 grams shredded chedda cheese",
+        "34 grams black olives",
+        "1 tablespoon cilantro"
+      ],
+
+      steps: [
+        '1Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dui risus, ullamcorper ut dolor eget, malesuada porta neque. Maecenas pharetra tristique nunc. Morbi lobortis imperdiet orci et ornare. Nulla sollicitudin in velit id mollis. Duis vehicula elit in justo blandit, ut congue magna tempor. Aliquam et ex augue. Duis quam lorem, fringilla at iaculis id, fringilla nec leo.',
+        '2Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dui risus, ullamcorper ut dolor eget, malesuada porta neque. Maecenas pharetra tristique nunc. Morbi lobortis imperdiet orci et ornare. Nulla sollicitudin in velit id mollis. Duis vehicula elit in justo blandit, ut congue magna tempor. Aliquam et ex augue. Duis quam lorem, fringilla at iaculis id, fringilla nec leo.',
+        '3Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dui risus, ullamcorper ut dolor eget, malesuada porta neque. Maecenas pharetra tristique nunc. Morbi lobortis imperdiet orci et ornare. Nulla sollicitudin in velit id mollis. Duis vehicula elit in justo blandit, ut congue magna tempor. Aliquam et ex augue. Duis quam lorem, fringilla at iaculis id, fringilla nec leo.',
+        '4Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dui risus, ullamcorper ut dolor eget, malesuada porta neque. Maecenas pharetra tristique nunc. Morbi lobortis imperdiet orci et ornare. Nulla sollicitudin in velit id mollis. Duis vehicula elit in justo blandit, ut congue magna tempor. Aliquam et ex augue. Duis quam lorem, fringilla at iaculis id, fringilla nec leo.'
+      ]
+    }
+  }
+
+  ngOnInit(): void { 
+    
   }
 
 }
