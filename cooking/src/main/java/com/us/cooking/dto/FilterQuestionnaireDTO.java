ee1b5e8de3a2c769afce3a886baf016b8ac78695
Dominik <dominik.rok4@gmail.com>
2021-03-25 14:06:06 +0100

    Dodanie bean validation

--- cooking/src/main/java/com/us/cooking/dto/FilterQuestionnaireDTO.java
+++ cooking/src/main/java/com/us/cooking/dto/FilterQuestionnaireDTO.java
@@ -2,8 +2,13 @@ package com.us.cooking.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class FilterQuestionnaireDTO {
+    @NotBlank
+    @Size(max = 300)
     private String cuisineTypeValue;
     private String levelOfCookingSkillValue;
     private String mealTypeValue;
