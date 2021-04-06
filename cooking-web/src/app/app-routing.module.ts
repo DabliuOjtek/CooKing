0ea553b9537ae6ec281766765249886fe0128340
Wojtek <matrix890098@gmail.com>
2021-04-06 20:06:54 +0200

    Dodanie routingów

--- cooking-web/src/app/app-routing.module.ts
+++ cooking-web/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
+import { PageNotFoundComponent } from './modules/pages/page-not-found/page-not-found.component';
+import { MainComponent } from './modules/pages/main/main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeComponent } from './modules/pages/recipe/recipe.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'recipe', component: RecipeComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [MainComponent, RecipeComponent, PageNotFoundComponent];
--- cooking-web/src/app/app.module.ts
+++ cooking-web/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { QuestionnaireComponent } from './modules/components/questionnaire/questionnaire.component';
-import { MainComponent } from './modules/pages/main/main.component';
 import { SliderComponent } from './modules/components/slider/slider.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './modules/components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CookingModule } from './modules/cooking.module';
-import { RecipeComponent } from './modules/pages/recipe/recipe.component';
 import { SliderItemDirective } from './modules/components/slider/slider-item.directive';
-import { TileComponent } from './modules/pages/tile/tile.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -18,11 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     NavbarComponent,
     QuestionnaireComponent,
-    RecipeComponent,
     SliderComponent,
     SliderItemDirective,
-    MainComponent,
-    TileComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
